ff3d61c9b6ef6d60ed0d30f379346540f00b0455
beco <aalcocer@gmail.com>
2022-07-21 01:17:32 -0500

    new configuration

--- config/app.php
+++ config/app.php
@@ -2,4 +2,7 @@
 
 return [
   'notion_token' => getenv('notion_token'),
+  'aws_key'      => getenv('aws_key'),
+  'aws_secret'   => getenv('aws_secret'),
+  'bucket_name'  => 'notion.static.b3co.com',
 ];
