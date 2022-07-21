2bc91c774a5c74057eaf18595c925f89c982d4a7
beco <aalcocer@gmail.com>
2022-07-21 11:55:20 -0500

    pagination enabled

--- objects/Notion.php
+++ objects/Notion.php
@@ -24,11 +24,14 @@ class Notion {
   private static $endpoints = [
     'get_page' => [
       'url' => 'https://api.notion.com/v1/pages/:page_id',
-      'params' => ['page_id']
+      'params' => ['page_id'],
+      'optional' => [],
     ],
     'get_children' => [
       'url' => 'https://api.notion.com/v1/blocks/:block_id/children',
-      'params' => ['block_id']
+      'params' => ['block_id'],
+      // will be appended as the GET part of the request
+      'optional' => ['start_cursor'],
     ],
   ];
 
@@ -37,7 +40,7 @@ class Notion {
   private $config;
 
   public function __construct($config = []) {
-    $this->token = getenv('notion_token') or die("no notion token");
+    $this->token = $config['notion_token'] or die("no notion token");
     $this->client = new Client();
   }
 
@@ -46,14 +49,41 @@ class Notion {
   }
 
   public function getNodesFrom($id) {
-    $content = $this->retrieve('get_children', ['block_id' => $id]);
-    return $content;
+    $blocks   = [];
+    $continue = false;
+    $cursor   = null;
+
+    $i = 0;
+
+    do {
+      $continue  = false;
+      $optionals = [];
+
+      if($cursor !== null) {
+        $optionals['start_cursor'] = $cursor;
+      }
+
+      $content = $this->retrieve('get_children', [
+        'block_id' => $id
+      ], $optionals);
+
+      $content = json_decode($content, true);
+      $blocks  = array_merge($blocks, $content['results']);
+
+      unset($content['results']);
+
+      if($content['has_more'] == 1) {
+        $cursor = $content['next_cursor'];
+        $continue = true;
+      }
+
+    } while($continue);
+    return $blocks;
   }
 
-  public function retrieve($endpoint, $params = []) {
-    $url = $this->populateUrl($endpoint, $params);
+  public function retrieve($endpoint, $params = [], $optionals = []) {
+    $url = $this->populateUrl($endpoint, $params, $optionals);
     try {
-      printf("token: %s\n", $this->token);
       $response = $this->client->request('GET', $url, [
         'headers' => [
           'Accept'         => 'application/json',
@@ -67,20 +97,31 @@ class Notion {
     return $response->getBody();
   }
 
-  private function populateUrl($endpoint, $params = []) {
+  private function populateUrl($endpoint, $params = [], $optionals = []) {
     $url = Notion::$endpoints[$endpoint]['url'];
+    $ps  = [];
+
     foreach(Notion::$endpoints[$endpoint]['params'] as $param) {
-      if($params[$param] != null) {
+      if($params[$param] !== null) {
         $url = preg_replace(sprintf('/\:%s/', $param), $params[$param], $url, 1);
       }
     }
-    return $url;
+
+    if(Notion::$endpoints[$endpoint]['optional'] !== null) {
+      foreach(Notion::$endpoints[$endpoint]['optional'] as $optional) {
+        if($optionals[$optional] !== null) {
+          $ps[] = sprintf("%s=%s", $optional, $optionals[$optional]);
+        }
+      }
+    }
+
+    return sprintf("%s?%s", $url, join("&", $ps));
   }
 
   public function getChildren($id, $page, $upload = false) {
-    $blocks = json_decode($this->getNodesFrom($id), true);
+    $blocks = $this->getNodesFrom($id);
     $children = [];
-    foreach($blocks['results'] as $block) {
+    foreach($blocks as $block) {
       if(Notion::$classes[$block['type']]) {
         $children[] = new Notion::$classes[$block['type']]($block, $page, $upload);
       } else {
