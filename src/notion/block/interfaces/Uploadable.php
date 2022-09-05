c3c33a3cb9ab585c009f692983e5931e9f24c231
beco <aalcocer@gmail.com>
2022-09-04 23:11:48 -0500

    removed interace in order for inheritance

--- src/notion/block/interfaces/Uploadable.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace b3co\notion\block\interfaces;
-
-interface Uploadable {
-  public function isUploaded():bool;
-  public function uploadToS3():bool;
-  public function getS3Key():string;
-}
