fa98dd0f418c7a859d9b20d9230a43e402a1f40a
beco <aalcocer@gmail.com>
2023-04-11 14:34:42 -0600

    adding /app dir with autoloader and config sample file

---
+++ app/config_sample.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+  'notion_token'  => '',
+  'aws_key'       => '',
+  'aws_secret'    => '',
+  'aws_region'    => 'us-east-2',
+  'bucket_name'   => '',
+  'templates_dir' => 'templates',
+];
